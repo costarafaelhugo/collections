f24d720c415884631a89fbfd744ae80b7007fcfe
Hugo Costa <costarafaelhugo@gmail.com>
2021-03-19 12:26:50 -0300

    sorted by
    Utilizei o sorted by para alinhar os salários e imprimir na ordem.

--- src/main/kotlin/me/user/TestList.kt
+++ src/main/kotlin/me/user/TestList.kt
@@ -1,18 +1,21 @@
 package me.user
 
 fun main() {
-    val joao = Funcionario("João", 1000.0)
-    val pedro = Funcionario("Pedro", 2000.0)
+    val joao = Funcionario("João", 2000.0)
+    val pedro = Funcionario("Pedro", 1500.0)
     val maria = Funcionario("Maria", 4000.0)
 
     val funcionarios = listOf(joao, pedro, maria)
 
-    funcionarios.forEach{ println(it) }
+    funcionarios.forEach { println(it) }
     println("------------")
 
     println(funcionarios.find { it.nome == "Maria" })
-
     println("------------")
+
+    funcionarios
+        .sortedBy { it.salario }
+        .forEach { println(it)}
 }
 
 data class Funcionario(
