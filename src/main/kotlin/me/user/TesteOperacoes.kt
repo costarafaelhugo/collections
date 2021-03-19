c9fc02852ea6abd643f3892dcb32b568a0b626da
Hugo Costa <costarafaelhugo@gmail.com>
2021-03-19 10:26:53 -0300

    Collections
    Aqui estão alguns modos de se utilizar array em Kotlin

---
+++ src/main/kotlin/me/user/TesteOperacoes.kt
@@ -0,0 +1,17 @@
+package me.user
+
+fun main() {
+    val salarios = doubleArrayOf(1000.0, 2250.0, 4000.0)
+
+    for (salario in salarios){
+        println(salario)
+    }
+    println("---------------------")
+    println("Maior salário: ${salarios.max()}")
+    println("Menor salário: ${salarios.min()}")
+    println("Média dos salários: ${salarios.average()}")
+
+    val salarioMaiorQue2500 = salarios.filter {it > 2500.0 }
+    println("-----------------")
+    salarioMaiorQue2500.forEach{println(it)}
+}
---
+++ src/main/kotlin/me/user/TetesArrayString.kt
@@ -0,0 +1,20 @@
+package me.user
+
+fun main() {
+    val nomes = Array(3) { "" }
+    nomes[0] = "Maria"
+    nomes[1] = "Zaza"
+    nomes[2] = "João"
+
+    for (nome in nomes) {
+        println(nome)
+    }
+    println("---------------")
+    nomes.sort()
+    nomes.forEach { println(it) }
+    println("---------------")
+
+    val nomes2 = arrayOf("Maria", "Zaza", "Pedro")
+    nomes2.sort()
+    nomes2.forEach { println(it) }
+    }
